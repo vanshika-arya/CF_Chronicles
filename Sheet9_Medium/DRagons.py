f643ecb23c42f3d5701fb8d145df9515f0f3ff03
Vanshika Arya <vanshika3915@gmail.com>
2026-04-14 19:14:32 +0530

    Dragons

---
+++ Sheet9_Medium/DRagons.py
@@ -0,0 +1,17 @@
+s, n = map(int, input().split())
+
+dragons = []
+for _ in range(n):
+    x, y = map(int, input().split())
+    dragons.append((x, y))
+
+dragons.sort()
+
+for x, y in dragons:
+    if s > x:
+        s += y
+    else:
+        print("NO")
+        break
+else:
+    print("YES")
